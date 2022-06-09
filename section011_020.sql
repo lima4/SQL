d73affa170dfeade5fa8ea0757c2111bdc7ff1c8
lima <chocgamoon95@gmail.com>
2022-06-09 19:50:58 +0900

    SQL

--- section011_020.sql
+++ section011_020.sql
@@ -77,7 +77,69 @@ SELECT ename, sal, job FROM emp WHERE job in ('SALESMAN', 'ANALYST','MANAGER');
 -- SALESMAN, ANALYST, MANAGER가 아닌 사원들의 이름, 월급, 직업 출력
 SELECT ename, sal, job FROM emp WHERE job NOT in ('SALESMAN', 'ANALYST', 'MANAGER');
 
--- 015
+-- 015 ( 논리연산자)
+
+-- 직업이 SALESMAN이고 월급이 1200이상인 사원들의 이름, 월급, 직업을 출력해보자
+SELECT ename, sal, job FROM emp 
+WHERE job='SALESMAN' AND sal >=1200;
+
+/*
+TRUE AND TRUE = TRUE
+TRUE AND FALSE = FALSE
+TURE AND NULL = NULL
+
+TRUE OR TRUE = TRUE
+TRUE OR FALSE = TRUE
+TRUE OR NULL = TRUE
+*/
+
+--016 대소문자 변환
+
+--사원 테이블의 이름을 출력하는데 첫 번째 컬럼은 이름을 대문자, 두 번째 컬럼은 이름을 소문자, 세 번째 컬럼은 이름의 첫 번째 철자는 대문자, 나머지는 소문자로 출력하기
+-- UPPER 대문자 변환, LOWER 소문자 변환, INITCAP 첫 번째 철자만 대문자
+SELECT UPPER(ename), LOWER(ename), INITCAP(ename) FROM emp;
+
+--017 특정 문자 추출하기
+
+--영어 단어 SMITH에서 SMI만 잘라내어 출력하기
+-- SQL은 첫번째가 1로 시작하므로 S 1 M 2 I 3 T 4 H 5이다 
+SELECT SUBSTR('SMITH', 1, 3) FROM DUAL;
+
+-- 영어 단어 SMITH에서 TH만 잘라서 추출
+-- -2자리인 T부터 2개의 단어 추출
+SELECT SUBSTR('SMITH', -2, 2) FROM DUAL;
+
+--MITH 추출하기
+-- 두번째 자리부터 전부 추출하려면 2라고 적는다. 
+SELECT SUBSTR('SMITH', 2) FROM DUAL;
+
+-- 018 문자열의 길이를 출력하기
+
+-- 이름을 출력하고 그 옆에 이름의 철자 개수를 출력
+-- LENGTH는 문자열의 길이를 출력하는 함수이다. 한글도 마찬가지로 길이가 출력된다.
+SELECT ename, LENGTH(ename) FROM emp;
+
+--019 문자에서 특정 철자의 위치 출력하기
+-- 사원 이름 SMITH에서 알파벳 철자 M이 몇 번째 자리에 있는지 출력
+-- INSTR 함수는 문자에서 특정 철자의 위치를 출력하는 함수이다. 
+SELECT INSTR('SMITH', 'M') FROM dual;
+
+-- 만약 abcdefgh@naver.com에서 naver.com을 출력하고 싶다면 
+-- 1. @의 위치 파악하기
+SELECT INSTR('abcdefg@naver.com', '@') FROM DUAL; --8번째 자리에 위치
+
+-- 2. 8+1 번째 자리부터 끝까지 출력하면 된다
+SELECT SUBSTR('abcdefg@naver.com', INSTR('abcdefg@naver.com' ,'@')+1) FROM DUAL;
+
+--020 특정 철자를 다른 철자로 변경하기
+
+-- 이름과 월급을 출력하는데, 월급을 출력할 때 숫자 0을 *로 출력하라
+-- REPLACE는 특정 철자를 다른 철자로 변경하는 문자 함수
+SELECT ename, REPLACE(sal, 0, '*') FROM emp;
+
+-- 숫자의 0, 1, 2, 3을 *로 출력하기
+-- REGEXP_REPLACE는 정규식 함수이다. 정규식 함수는 일반 함수보다 더 복잡한 형태의 검색 패턴으로 데이터를 조회할 수 있다.
+SELECT ename, REGEXP_REPLACE(sal, '[0-3]', '*') FROM emp;
 
 
 
